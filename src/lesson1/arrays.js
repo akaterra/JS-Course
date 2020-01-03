d943858eccd2e7d4ce14f161014d912efd3d0692
Your Name <you@example.com>
2020-01-03 15:20:43 +0200

    initial commit

--- src/lesson1/arrays.js
+++ src/lesson1/arrays.js
@@ -1,25 +1,9 @@
 function filterNumbersAndMultiplyBy(array, multiplier) {
-  if (typeof multiplier !== 'number') {
-    throw new Error('multiplier should be a number');
-  }
-
-  if (!Array.isArray(array)) {
-    throw new Error('array expected');
-  }
-
-  return array.filter((item) => typeof item === 'number').map((item) => item * 2);
+  // write code here
 }
 
 function filterNumbersDivisibleBy(array, divisor) {
-  if (typeof divisor !== 'number') {
-    throw new Error('divisor should be a number');
-  }
-
-  if (!Array.isArray(array)) {
-    throw new Error('array expected');
-  }
-
-  return array.filter((item) => typeof item === 'number' && item % divisor === 0);
+  // write code here
 }
 
 module.exports = {
